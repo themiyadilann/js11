2902cf0b9c6d80b075155495f1d68b9f7a84229c
themiyadilann <128908885+themiyadilann@users.noreply.github.com>
2023-09-16 23:58:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hellow My Feriends");
+
+var x = 3;
+console.log(x)
+x = x + 6;
+console.log(x)
+x = x + 6;
+console.log(x)
+x++ ;
+console.log(x)
+
+let q = 3;
+console.log(q)
+q = q + 6;
+console.log(q)
+q = q + 6;
+console.log(q)
+q++ ;
+console.log(q)
+const c = 5;
+console.log(c)
+
+let p = 7;
+let sum = p + 2;
+console.log(sum);
